a0ef60202cf470cdc6e79a97a8f2d88e32606b88
Yifan Zhang <freqyifan@gmail.com>
2015-05-13 10:06:40 +0300

    :hammer: fix a problem where '~' doesn't work

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-if [[ ! -e "~/.vim/bundle/Vundle.vim" ]];
+if [[ ! -d "$HOME/.vim/bundle/Vundle.vim" ]];
 then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
