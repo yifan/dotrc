622698d59c6d405876ef2a46b0276a11557d025c
Yifan Zhang <freqyifan@gmail.com>
2015-06-18 20:10:57 +0300

    slate installation

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ if [[ ! -e "$HOME/.vim/bundle/Vundle.vim" ]];
 then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
-curl -O https://github.com/mbadolato/iTerm2-Color-Schemes/raw/master/schemes/Cobalt2.itermcolors
 ln -s ~/dotrc/vimrc ~/.vimrc
+ln -s ~/dotrc/slate.js ~/.slate.js
