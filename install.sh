479d0ec9b34fd398bb8c18f24765105f7d4470d6
Yifan Zhang <freqyifan@gmail.com>
2015-12-22 10:40:17 +0300

    changed config to plain text and fixed layouts

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ test -d ~/.vim/colors || mkdir -p ~/.vim/colors
 test -e ~/.vim/colors/distinguished.vim || curl -o ~/.vim/colors/distinguished.vim https://github.com/Lokaltog/vim-distinguished/raw/develop/colors/distinguished.vim
 test -e ~/.vimrc || ln -s ~/dotrc/vimrc ~/.vimrc
 test -e ~/.gitconfig || ln -s ~/dotrc/gitconfig ~/.gitconfig
-test -e ~/.slate.js || ln -s ~/dotrc/slate.js ~/.slate.js
+test -e ~/.slate || ln -s ~/dotrc/slate ~/.slate
+test -e ~/.bash_profile || ln -s ~/dotrc/bash_profile ~/.bash_profile
