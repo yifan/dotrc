b7e7d9f98ce0b74a8dc1e6929b2c700816108bf9
Yifan Zhang <freqyifan@gmail.com>
2016-03-23 11:47:42 +0300

    to work with github redirection issue

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 test -e "$HOME/.vim/bundle/Vundle.vim" ||  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 test -d ~/.vim/colors || mkdir -p ~/.vim/colors
-test -e ~/.vim/colors/distinguished.vim || curl -o ~/.vim/colors/distinguished.vim https://github.com/Lokaltog/vim-distinguished/raw/develop/colors/distinguished.vim
+test -e ~/.vim/colors/distinguished.vim || curl -o ~/.vim/colors/distinguished.vim -L https://github.com/Lokaltog/vim-distinguished/raw/develop/colors/distinguished.vim
 test -e ~/.vimrc || ln -s ~/dotrc/vimrc ~/.vimrc
 test -e ~/.gitconfig || ln -s ~/dotrc/gitconfig ~/.gitconfig
 test -e ~/.slate || ln -s ~/dotrc/slate ~/.slate
